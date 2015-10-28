910a074611ade167d7654f1f8fad969210fd4353
Artit Jirapatnakul <artitj@gmail.com>
2015-10-28 20:34:20 +0000

    Part 1 and 2 done

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,29 +3,69 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  sum = 0
+  unless arr.empty?
+    arr.each { |x| sum += x}
+  end
+  return sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  sum = 0
+  unless arr.empty?
+    if arr.length < 2
+      sum = arr[0]
+    else
+      sorted_arr = arr.sort.reverse
+      sum = sorted_arr[0] + sorted_arr[1]
+    end
+  end
+  return sum
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  sum = 0
+  result = false
+  unless arr.empty?
+    if arr.length < 2
+      sum = arr[0]
+    else
+      arr.permutation do |x|
+        sum = x[0] + x[1]
+        if sum == n
+          result = true
+        end
+      end
+    end
+  end
+  return result
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  lowercase_s = s.downcase
+  result = false
+  if lowercase_s =~ /\A(?=[^aeiou])(?=[a-z])/i
+    result = true
+  end
+  return result
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+
+  result = false
+  if s =~ /\A[0-1]+/ 
+    num = s.to_i(2)
+    if num % 4 == 0
+      result = true
+    end
+  end
+  return result
 end
 
 # Part 3
